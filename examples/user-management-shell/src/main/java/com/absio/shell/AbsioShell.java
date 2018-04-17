420185a3b54cf52334f1d9b671935bd8f0ffb0a2
Nick Johnson <nick.johnson@absio.com>
2018-04-17 00:49:43 -0400

    Reorganized the shell classes to be a guided tour of the commands

--- examples/user-management-shell/src/main/java/com/absio/shell/AbsioShell.java
+++ examples/user-management-shell/src/main/java/com/absio/shell/AbsioShell.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.security.Security;
 
 @SpringBootApplication
-public class UserManagementShell {
+public class AbsioShell {
 
     public static void main(String[] args) {
         Security.insertProviderAt(new OpenSSLProvider(), 1);
-        SpringApplication.run(UserManagementShell.class, args);
+        SpringApplication.run(AbsioShell.class, args);
     }
 }
---
+++ examples/user-management-shell/src/main/java/com/absio/shell/PublicKeyCommands.java
@@ -0,0 +1,54 @@
+package com.absio.shell;
+
+import com.absio.broker.BrokerException;
+import com.absio.crypto.key.IndexedECPublicKey;
+import com.absio.crypto.key.KeyType;
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.*;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+@ShellComponent
+@ShellCommandGroup("3. Public Key Commands")
+public class PublicKeyCommands {
+    @ShellMethodAvailability("*")
+    public Availability publicKeyMethodAvailabilityCheck() {
+        return AbsioServerProvider.INSTANCE.isAuthenticated() ? Availability.available() : Availability.unavailable("The session must be authenticated.");
+    }
+
+    @ShellMethod("Retrieve a user's public key by type and index.")
+    public String getPublicKeyByIndex(String userId, String keyType, int index) throws InterruptedException, BrokerException, IllegalAccessException, IOException {
+        IndexedECPublicKey publicKey = AbsioServerProvider.INSTANCE.getPublicKeyIndex(UUID.fromString(userId), KeyType.findByName(keyType), index);
+
+        return publicKeyToString(publicKey);
+    }
+
+    private static String publicKeyToString(IndexedECPublicKey publicKey) {
+        return String.format("%s Public key %s : %s : %b : %s", publicKey.getKeyType(), publicKey.getIndex(), publicKey.isActive() ? "active" : "inactive", publicKey.getAlgorithm(), DatatypeConverter.printHexBinary(publicKey.getEncoded()));
+    }
+
+    @ShellMethod("Retrieve a user's latest active key by type.")
+    public String getPublicKeyLatestActive(String userId, String keyType) throws InterruptedException, BrokerException, IllegalAccessException, IOException {
+        IndexedECPublicKey publicKey = AbsioServerProvider.INSTANCE.getPublicKeyLatestActive(UUID.fromString(userId), KeyType.findByName(keyType));
+
+        return publicKeyToString(publicKey);
+    }
+
+    @ShellMethod("Retrieves a list of public keys filterable by user, type, and index.")
+    public String getPublicKeyList(@ShellOption(defaultValue = ShellOption.NULL) String userId, @ShellOption(defaultValue = ShellOption.NULL) String keyType, @ShellOption(defaultValue = ShellOption.NULL) Integer index) throws InterruptedException, BrokerException, IllegalAccessException, IOException {
+        UUID userUUID = userId != null ? UUID.fromString(userId) : null;
+        KeyType type = keyType != null ? KeyType.findByName(keyType) : null;
+        List<IndexedECPublicKey> publicKeyList = AbsioServerProvider.INSTANCE.getPublicKeyList(userUUID, type, index);
+
+        StringBuilder publicKeys = new StringBuilder();
+        for (IndexedECPublicKey publicKey : publicKeyList) {
+            publicKeys.append(publicKeyToString(publicKey));
+            publicKeys.append("\n");
+        }
+
+        return publicKeys.toString();
+    }
+}
